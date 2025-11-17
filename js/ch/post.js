215bef0808d77c11f9a3f10bd816ef9968de6bbd
游隼网络 <143577882+Yousun-Network@users.noreply.github.com>
2025-11-17 14:27:10 +0800

    Update post.js

--- js/ch/post.js
+++ js/ch/post.js
@@ -73,41 +73,123 @@ function getaAssetsByCoincap(coincap){
 	return _data;
 }
 
-function getaAssetsBySymbol(symbol){
-	 let _url = "https://api.coincap.io/v2/assets?search=" + symbol
-	 let r_data;
-	 $.ajax({
-	 	url: _url,
-	 	type:"GET",
-	 	async: false,
-	 	success:function (data) {
-	 		console.log(data);
-			if(notNull(data)){
-				let coincap = data.data[0].id;
-				let url2 = "https://api.coincap.io/v2/assets/" + coincap;
-				$.ajax({
-					url: url2,
-					type:"GET",
-					async: false,
-					success:function (data2) {
-						console.log(data2);
-						r_data = data2;
-					},
-					dataType : "json",
-					error: function(err2){
-						console.log(err2);
-					}
-				});
-			}
-	 	},
-	 	dataType : "json",
-	 	error: function(err){
-	 		console.log(err);
-	 	}
-	 });
-	 return r_data;
-}
+// function getaAssetsBySymbol(symbol){
+// 	 let _url = "https://api.coincap.io/v2/assets?search=" + symbol
+// 	 let r_data;
+// 	 $.ajax({
+// 	 	url: _url,
+// 	 	type:"GET",
+// 	 	async: false,
+// 	 	success:function (data) {
+// 	 		console.log(data);
+// 			if(notNull(data)){
+// 				let coincap = data.data[0].id;
+// 				let url2 = "https://api.coincap.io/v2/assets/" + coincap;
+// 				$.ajax({
+// 					url: url2,
+// 					type:"GET",
+// 					async: false,
+// 					success:function (data2) {
+// 						console.log(data2);
+// 						r_data = data2;
+// 					},
+// 					dataType : "json",
+// 					error: function(err2){
+// 						console.log(err2);
+// 					}
+// 				});
+// 			}
+// 	 	},
+// 	 	dataType : "json",
+// 	 	error: function(err){
+// 	 		console.log(err);
+// 	 	}
+// 	 });
+// 	 return r_data;
+// }
+/**
+ * 【已修改】: 使用 api.coingecko.com 替代 api.coincap.io
+ * * 通过币种符号（如 "ETH"）获取市场数据。
+ * 注意：此函数保持了原始的同步调用 (async: false)，这会冻结浏览器，
+ * 在实际生产中强烈不推荐，但为了兼容你现有的代码结构，此处予以保留。
+ */
+function getaAssetsBySymbol(symbol) {
+	let r_data; // 最终返回的数据
+	let coin_id; // CoinGecko 的币种 ID (例如 "ethereum")
 
+	// --- 步骤 1: 使用 CoinGecko 的 search API 查找 coin_id ---
+	// 对应原 CoinCap 的 /assets?search=...
+	let search_url = "https://api.coingecko.com/api/v3/search?query=" + symbol;
+
+	$.ajax({
+		url: search_url,
+		type: "GET",
+		async: false, // 保持同步调用
+		success: function(searchData) {
+			console.log("CoinGecko Search Data:", searchData);
+			if (searchData && searchData.coins && searchData.coins.length > 0) {
+				
+				// 优化：优先查找符号(symbol)完全匹配的
+				let exactMatch = searchData.coins.find(coin => coin.symbol.toLowerCase() === symbol.toLowerCase());
+				
+				if (exactMatch) {
+					coin_id = exactMatch.id;
+				} else {
+					// 如果没有完全匹配的，则退而求其次，使用第一个搜索结果
+					coin_id = searchData.coins[0].id;
+				}
+				console.log("CoinGecko 匹配到的 ID:", coin_id);
+			}
+		},
+		dataType: "json",
+		error: function(err) {
+			console.error("CoinGecko Search API 失败:", err);
+		}
+	});
+
+	// 如果步骤 1 失败，无法获取 coin_id，则直接返回
+	if (!coin_id) {
+		console.error("CoinGecko 未能找到 " + symbol + " 的 ID");
+		// 返回一个与原函数失败时类似的 undefined
+		return undefined; 
+	}
+
+	// --- 步骤 2: 使用 coin_id 获取市场数据 ---
+	// 对应原 CoinCap 的 /assets/ID
+	let market_url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=" + coin_id;
+
+	$.ajax({
+		url: market_url,
+		type: "GET",
+		async: false, // 保持同步调用
+		success: function(marketData) {
+			console.log("CoinGecko Market Data:", marketData);
+			if (marketData && marketData.length > 0) {
+				
+				// 从 CoinGecko 获取 'current_price'
+				let price = marketData[0].current_price; 
+
+				// --- 步骤 3: 格式化返回数据 ---
+				// 将 CoinGecko 的 price 封装成原 CoinCap 的数据结构
+				// 你的调用代码期望: data.data.vwap24Hr
+				// 我们将 price 赋值给 vwap24Hr
+				r_data = {
+					"data": {
+						"vwap24Hr": price
+					}
+				};
+				console.log("格式化后的 r_data:", r_data);
+			}
+		},
+		dataType: "json",
+		error: function(err) {
+			console.error("CoinGecko Market API 失败:", err);
+		}
+	});
+
+	// 返回格式化后的数据 (如果API调用失败，这里将返回 undefined)
+	return r_data;
+}
 function post(_url,_data,_async,_fnc,_that){
 	let r_data;
 	if(isNull(_async) && _async!= false){
